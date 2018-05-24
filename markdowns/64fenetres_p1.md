4fc79b6d28e3f7e85234acbf250464568ee9ab82
richard urunuela <ierdre44@gmail.com>
2018-05-24 16:51:08 +0200

    ajout Exo

--- markdowns/64fenetres_p1.md
+++ markdowns/64fenetres_p1.md
@@ -2,6 +2,8 @@
 
 [![Présentation](https://www.dropbox.com/s/vzfx3bjms0xieut/screen-video.jpg?raw=1)](https://www.dropbox.com/s/qs6u7yx71i4yub0/64fenetres_-_moyenne.mov?raw=0)
 
+@[Exercice 0]({"stubs": ["Sources/swift_template_fenetre.swift"], "command": "/bin/bash run0.sh"})
+
 # Exercice 1
 
 @[Exercice 1]({"stubs": ["Sources/swift_template_fenetre.swift"], "command": "/bin/bash run1.sh"})
--- swift-template/Tests/swift-templateTests/swift_templateTests.swift
+++ swift-template/Tests/swift-templateTests/swift_templateTests.swift
@@ -16,6 +16,9 @@ class swift_templateTests: XCTestCase {
         XCTAssertEqual(swift_template_fenetre.solve(fenetres:fenetres),["G", "O", "F", "G", "F", "F", "F", "O", "G", "F", "F", "F", "F", "F", "F", "G", "F", "O", "F", "F", "F", "F", "F", "F", "G", "F", "F", "F", "F", "F", "F", "O", "F", "F", "F", "G", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "G", "O", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "F", "G"])
         
     }
+     func testExample0() {
+        XCTAssertEqual(true,true)
+    }
     //Exercice 2
     func testExample2() {
         var fenetres = Array(repeating: "F", count: 64)
