8875733c8d1bbd8ef395f13110b25fdba86afbe4
richard urunuela <ierdre44@gmail.com>
2018-05-24 16:59:59 +0200

    ajout Exo

--- swift-template/Tests/swift-templateTests/swift_templateTests.swift
+++ swift-template/Tests/swift-templateTests/swift_templateTests.swift
@@ -35,37 +35,15 @@ class swift_templateTests: XCTestCase {
     }
     override func tearDown() {
         super.tearDown()
-        if Path.exists("/project/target/Sources/swift_template.swift") {
-            do {
-                if testRun!.failureCount > 0 {
-                    print("TECHIO> message --channel Oops! Try Again 🐞");
-                    print("TECHIO> message --channel 'Hint 💡' Did you properly accumulate all stars into 'totalStars'? 🤔");
-
-                } else {
-                    let content = try File.read(atPath: "/project/target/Sources/swift_template.swift")
-                    if content.range(of:"galaxies.reduce") != nil { 
-			print("TECHIO> message --channel 'My personal Yoda, you are. 🙏' \"* ● ¸ .　¸. :° ☾ ° 　¸. ● ¸ .　　¸.　:. • \"");
-			print("TECHIO> message --channel 'My personal Yoda, you are. 🙏' \"           　★ °  ☆ ¸. ¸ 　★　 :.　 .   \"");
-			print("TECHIO> message --channel 'My personal Yoda, you are. 🙏' \"__.-._     ° . .　　　　.　☾ ° 　. *   ¸ .\"");
-			print("TECHIO> message --channel 'My personal Yoda, you are. 🙏' \"'-._\\7'      .　　° ☾  ° 　¸.☆  ● .　　　\"");
-			print("TECHIO> message --channel 'My personal Yoda, you are. 🙏' \" /'.-c    　   * ●  ¸.　　°     ° 　¸.    \"");
-			print("TECHIO> message --channel 'My personal Yoda, you are. 🙏' \" |  /T      　　°     ° 　¸.     ¸ .　　  \"");
-			print("TECHIO> message --channel 'My personal Yoda, you are. 🙏' \"_)_/LI\"\r\n");
-                    }
-                    else {
-                        print("TECHIO> message --channel 'Kudos 🌟' Good Job! Try using array.reduce")
-                    }
-                }
-            } catch {
-                print(error)
-            }
-        }
+        
         
     }
 
     static var allTests = [
-        ("testExample", testExample),
-          ("testExample2", testExample2),
-            ("testExample3", testExample3)
+         ("testExample0", testExample0),
+         ("testExample", testExample),
+         ("testExample1", testExample1),
+         ("testExample2", testExample2),
+         ("testExample3", testExample3)
     ]
 }
