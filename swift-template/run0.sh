5ebd0a332a11f1d428cfc116e0a4884737f2bae4
richard urunuela <ierdre44@gmail.com>
2018-05-24 19:04:50 +0200

    ajout Exo

--- swift-template/run0.sh
+++ swift-template/run0.sh
@@ -1,7 +1,6 @@
 swift package --version 
 echo "TECHIO> message DEB "
 swift build 1>/dev/null 
-swift-test -l 
-#swift-test --help 
+swift-test --help 
 #swift test -s testExample0  
 echo "TECHIO> ERROR "
